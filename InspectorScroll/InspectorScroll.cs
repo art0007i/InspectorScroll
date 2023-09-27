f34f496817147ee3639cd3302f576c0598cf75a7
art0007i <art0007i@gmail.com>
2023-09-27 23:56:43 +0200

    update to resonite (fix #2)

--- InspectorScroll/InspectorScroll.cs
+++ InspectorScroll/InspectorScroll.cs
@@ -1,20 +1,14 @@
 ﻿using HarmonyLib;
-using NeosModLoader;
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+using ResoniteModLoader;
 using FrooxEngine;
-using FrooxEngine.LogiX;
 
 namespace InspectorScroll
 {
-    public class InspectorScroll : NeosMod
+    public class InspectorScroll : ResoniteMod
     {
         public override string Name => "InspectorScroll";
         public override string Author => "art0007i";
-        public override string Version => "1.0.2";
+        public override string Version => "2.0.0";
         public override string Link => "https://github.com/art0007i/InspectorScroll/";
 
 		[AutoRegisterConfigKey]
@@ -28,19 +22,19 @@ namespace InspectorScroll
 			config = GetConfiguration();
         }
 
-		[HarmonyPatch(typeof(CommonTool))]
+		[HarmonyPatch(typeof(InteractionHandler))]
 		[HarmonyPatch("OnInputUpdate")]
-		class CommonTool_OnInputUpdate_Patch
+		class InteractionHandler_OnInputUpdate_Patch
 		{
-			public static void Postfix(CommonTool __instance)
+			public static void Postfix(InteractionHandler __instance)
 			{
 				IAxisActionReceiver axisActionReceiver = __instance.Laser.CurrentTouchable as IAxisActionReceiver;
 				if (axisActionReceiver != null)
 				{
-					if (((__instance.ActiveToolTip != null && __instance.ActiveToolTip.UsesSecondary) || __instance.InputInterface.GetControllerNode(__instance.Side).GetType() == typeof(IndexController)) && !__instance.InputInterface.ScreenActive)
+					if (((__instance.ActiveTool != null && __instance.ActiveTool.UsesSecondary) || __instance.InputInterface.GetControllerNode(__instance.Side).GetType() == typeof(IndexController)) && !__instance.InputInterface.ScreenActive)
 					{
-						var val = (AccessTools.Field(__instance.GetType(), "_inputs").GetValue(__instance) as CommonToolInputs).Axis.Value.Value;
-						val *= new BaseX.float2(-1, 1);
+						var val = (AccessTools.Field(__instance.GetType(), "_inputs").GetValue(__instance) as InteractionHandlerInputs).Axis.Value.Value;
+						val *= new Elements.Core.float2(-1, 1);
 						axisActionReceiver.ProcessAxis(__instance.Laser.TouchSource, val * config.GetValue(KEY_SPEED));
 					}
 				}
--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # InspectorScroll
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows scrolling inspectors using your thumbstick / touchpad.<br>
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that allows scrolling inspectors (and other ui panels) using your thumbstick / touchpad.<br>
 May not work on all controllers, if anything breaks please contact me on discord @ art0007i#2305<br>
 Tested to work on Oculus Quest 2 controllers.
 
-This mod implements the feature from [#1470](https://github.com/Neos-Metaverse/NeosPublic/issues/1470)
-
 ## Installation
-1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-1. Place [InspectorScroll.dll](https://github.com/art0007i/InspectorScroll/releases/latest/download/InspectorScroll.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [InspectorScroll.dll](https://github.com/art0007i/InspectorScroll/releases/latest/download/InspectorScroll.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
