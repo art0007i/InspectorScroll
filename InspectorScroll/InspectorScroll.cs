e4a5e4abd520de127a12084aeda6d43de0dc5b70
art0007i <art0007i@gmail.com>
2026-04-29 03:32:18 +0200

    rework a bunch

--- InspectorScroll/InspectorScroll.cs
+++ InspectorScroll/InspectorScroll.cs
@@ -1,41 +1,121 @@
-﻿using HarmonyLib;
+﻿using System.Reflection;
+using Elements.Core;
+using HarmonyLib;
 using ResoniteModLoader;
 using FrooxEngine;
+using Renderite.Shared;
 
-namespace InspectorScroll
+namespace InspectorScroll;
+
+public class InspectorScroll : ResoniteMod
 {
-    public class InspectorScroll : ResoniteMod
-    {
-        public override string Name => "InspectorScroll";
-        public override string Author => "art0007i";
-        public override string Version => "2.0.0";
-        public override string Link => "https://github.com/art0007i/InspectorScroll/";
+	public override string Name => "InspectorScroll";
+	public override string Author => "art0007i";
+	public override string Version => "2.0.2";
+	public override string Link => "https://github.com/art0007i/InspectorScroll/";
 
-		[AutoRegisterConfigKey]
-		public static ModConfigurationKey<float> KEY_SPEED = new ModConfigurationKey<float>("scroll_speed", "How fast you scroll, default is 120.", () => 120);
-		public static ModConfiguration config;
+	[AutoRegisterConfigKey]
+	public static ModConfigurationKey<float> KEY_SPEED = new("scroll_speed", "How fast you scroll, default is 120.", () => 120);
+	[AutoRegisterConfigKey]
+	public static ModConfigurationKey<bool> KEY_INDEX_JOYSTICK = new("index_joystick", "Use the joystick on index controller.", () => false);
+	[AutoRegisterConfigKey]
+	public static ModConfigurationKey<bool> KEY_BLOCK_INPUT_WORLD = new("block_input_world", "Prevent moving when hovering over a scrollable element in world space.", () => false);
+	[AutoRegisterConfigKey]
+	public static ModConfigurationKey<bool> KEY_BLOCK_INPUT_USER = new("block_input_user", "Prevent moving when hovering over a scrollable element in userspace.", () => false);
+	public static ModConfiguration config;
 
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("me.art0007i.InspectorScroll");
-            harmony.PatchAll();
-			config = GetConfiguration();
-        }
+	public override void OnEngineInit()
+	{
+		Harmony harmony = new Harmony("me.art0007i.InspectorScroll");
+		harmony.PatchAll();
+		config = GetConfiguration();
+	}
 
-		[HarmonyPatch(typeof(InteractionHandler))]
-		[HarmonyPatch("OnInputUpdate")]
-		class InteractionHandler_OnInputUpdate_Patch
+	[HarmonyPatch(typeof(InteractionHandler))]
+	[HarmonyPatch("OnInputUpdate")]
+	class InteractionHandler_OnInputUpdate_Patch
+	{
+		public static void Postfix(InteractionHandler __instance)
 		{
-			public static void Postfix(InteractionHandler __instance)
+			if (__instance.InputInterface.ScreenActive) return;
+				
+			if (__instance.Inputs.Axis.RegisterBlocks)
 			{
-				IAxisActionReceiver axisActionReceiver = __instance.Laser.CurrentTouchable as IAxisActionReceiver;
-				if (axisActionReceiver != null)
+				float2 val;
+				if (config.GetValue(KEY_INDEX_JOYSTICK) && 
+				    __instance.InputInterface.GetControllerNode(__instance.Side) is IndexController index)
 				{
-					if (((__instance.ActiveTool != null && __instance.ActiveTool.UsesSecondary) || __instance.InputInterface.GetControllerNode(__instance.Side).GetType() == typeof(IndexController)) && !__instance.InputInterface.ScreenActive)
+					val = index.Joystick.Value;
+				}
+				else
+				{
+					val = __instance.Inputs.Axis.Value.Value;
+				}
+				val *= new float2(-1, 1);
+				
+				IAxisActionReceiver? axisActionReceiver = __instance.Laser.CurrentTouchable as IAxisActionReceiver;
+				axisActionReceiver?.ProcessAxis(__instance.Laser.TouchSource, val * config.GetValue(KEY_SPEED));
+			}
+		}
+	}
+	
+	private static readonly FieldInfo InteractionHandlerInputs =
+		AccessTools.Field(typeof(InteractionHandler), "_inputs");
+
+	static bool ShouldAttemptInputBlock()
+	{
+		return config.GetValue(KEY_BLOCK_INPUT_WORLD) || config.GetValue(KEY_BLOCK_INPUT_USER);
+	}
+	
+	static bool CanScroll(InteractionHandler? __instance)
+	{
+		if (__instance == null) return false;
+		var axisActionReceiver = __instance.Laser.CurrentTouchable as IAxisActionReceiver;
+		if (axisActionReceiver == null) return false;
+
+		var block = ShouldAttemptInputBlock() &&
+		            axisActionReceiver.ProcessAxis(__instance.Laser.TouchSource, float2.Zero);
+
+		return block;
+	}
+	
+	[HarmonyPatch(typeof(InteractionHandler))]
+	[HarmonyPatch("BeforeInputUpdate")]
+	[HarmonyAfter("U-xyla.XyMod", "owo.Nytra.NoTankControls")]
+	class InputBlockPatch
+	{
+		static InteractionHandler? userSpaceHandlerLeft = null;
+		static InteractionHandler? userSpaceHandlerRight = null;
+		
+		// some code from Nytra's [NoTankControls fork](https://github.com/Nytra/NoTankControls)
+		private static void Postfix(InteractionHandler __instance)
+		{
+			if (!ShouldAttemptInputBlock()) return;
+			
+			if (__instance.World == Engine.Current.WorldManager.FocusedWorld)
+			{
+				var worldScroll = config.GetValue(KEY_BLOCK_INPUT_WORLD) && CanScroll(__instance);
+				var userScroll = (__instance.Side == Chirality.Left && CanScroll(userSpaceHandlerLeft))
+				                  || (__instance.Side == Chirality.Right && CanScroll(userSpaceHandlerRight));
+				if (worldScroll || (config.GetValue(KEY_BLOCK_INPUT_USER) && userScroll))
+				{
+					__instance.Inputs.Axis.RegisterBlocks = true;
+				}
+			}
+			else if (__instance.World == Userspace.UserspaceWorld)
+			{
+				if (__instance.Side == Chirality.Left)
+				{
+					if (userSpaceHandlerLeft.FilterWorldElement() == null)
 					{
-						var val = (AccessTools.Field(__instance.GetType(), "_inputs").GetValue(__instance) as InteractionHandlerInputs).Axis.Value.Value;
-						val *= new Elements.Core.float2(-1, 1);
-						axisActionReceiver.ProcessAxis(__instance.Laser.TouchSource, val * config.GetValue(KEY_SPEED));
+						userSpaceHandlerLeft = __instance;
+					}
+				}
+				else
+				{
+					if (userSpaceHandlerRight.FilterWorldElement() == null)
+					{
+						userSpaceHandlerRight = __instance;
 					}
 				}
 			}
--- InspectorScroll/Properties/AssemblyInfo.cs
+++ InspectorScroll/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("InspectorScroll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("art0007i")]
 [assembly: AssemblyProduct("InspectorScroll")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.2")]
